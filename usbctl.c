abae00c7e1231a04f8f0f64474f93a48ff1eadc6
Joachim Nilsson <troglobit@gmail.com>
2005-02-25 07:56:29 +0000

    More functionality

--- usbctl.c
+++ usbctl.c
@@ -10,8 +10,16 @@
 
 #include <argp.h>
 #include <stdio.h>
+#include <errno.h>
+#include <err.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <usb.h>
+
+#include "usbmisc.h"
+#include "usbext.h"
+
 
 const char *argp_program_version = "$Id$";
 const char *argp_program_bug_address = "<joachim.nilsson@afconsult.com>";
@@ -19,67 +27,111 @@ const char *argp_program_bug_address = "<joachim.nilsson@afconsult.com>";
 static char doc[] =
   "short program to show the use of argp\nThis program does little";
 
-static char args_doc[] = "COMMAND";
-
-int verbose = 0;
+static char args_doc[] = "[SHOW|RESET|STATUS]";
 
 /* initialise an argp_option struct with the options we except */
 static struct argp_option options[] =
   {
-    {"verbose", 'v', 0,      0, "Produce verbose output" },
-    {"device",  'D', "PATH", 0, "Use this device, instead of $DEVICE" },
+    {"verbose", 'v', 0,         0, "Produce verbose output" },
+    {"device",  'D', "PATH",    0, "Operate on this device, /proc/bus/usb/BBB/DDD ,instead of $DEVICE" },
+    {"find",    'd', "VID/PID", 0, "Operate on a list of devices matching VendorID/DeviceID"},
     { 0 }
   };
 
 /* Used by `main' to communicate with `parse_opt'. */
 struct arguments
 {
-  char *cmd[1];
-  int silent, verbose;
-  char *device_path;
+      char *cmd[1];
+      int silent, verbose;
+      int vid, pid;
+      char *path;
 };
 
 /* Parse a single option. */
 static error_t
 parse_opt (int key, char *arg, struct argp_state *state)
 {
-  /* Get the INPUT argument from `argp_parse', which we
-     know is a pointer to our arguments structure. */
-  struct arguments *arguments = state->input;
+   char *tmp;
+   /* Get the INPUT argument from `argp_parse', which we
+      know is a pointer to our arguments structure. */
+   struct arguments *args = state->input;
 
-  switch (key)
-    {
-    case 'q': case 's':
-      arguments->silent = 1;
-      break;
+   switch (key)
+   {
+      case 'q': case 's':
+         args->silent = 1;
+         break;
 
-    case 'v':
-      arguments->verbose = 1;
-      break;
+      case 'v':
+         args->verbose ++;
+         usb_set_debug (args->verbose);
+         break;
 
-    case 'D':
-      arguments->device_path = arg;
-      break;
+      case 'd':
+#if 0
+         /* Either VID/PID or only a VID */
+         tmp = strtok (arg, "/\0");
+         if (tmp)
+         {
+            /* Convert one record, abort on error. */
+            if (1 != sscanf (tmp, "0x%X", &args->vid))
+            {
+               errx (EINVAL, "%s is not a valid VID.", arg);
+            }
 
-    case ARGP_KEY_ARG:
-      if (state->arg_num >= 1)
-        /* Too many arguments. */
-        argp_usage (state);
+            /* PID is actually optional. */
+            tmp = strtok (NULL, "\0");
+            if (tmp)
+            {
+               /* Convert one record, abort on error. */
+               if (1 != sscanf (tmp, "0x%X", &args->pid))
+               {
+                  errx (EINVAL, "%s is not a valid PID.", arg);
+               }
+            }
+            else
+            {
+               printf ("No PID given, will result in listing ALL devices for that vendor.\n");
+            }
+         }
+         else
+         {
+            errx (EINVAL, "%s is not a valid VID/PID pair.", arg);
+         }
+#else
+         /* Alternative implementation, stolen from lsusb */
+         tmp = strchr (arg, '/');
+         if (tmp) *tmp++ = 0;
+         args->vid = strtoul (arg, NULL, 0);
+         if (tmp) args->pid = strtoul (tmp, NULL, 0);
+#endif
+         break;
 
-      arguments->cmd[state->arg_num] = arg;
+      case 'D':
+         args->path = arg;
+         break;
 
-      break;
+      case ARGP_KEY_ARG:
+         if (state->arg_num >= 1)
+            /* Too many arguments. */
+            argp_usage (state);
 
-    case ARGP_KEY_END:
-      if (state->arg_num < 1)
-        /* Not enough arguments. */
-        argp_usage (state);
-      break;
+         args->cmd[state->arg_num] = arg;
 
-    default:
-      return ARGP_ERR_UNKNOWN;
-    }
-  return 0;
+         break;
+
+      case ARGP_KEY_END:
+         break;
+         if (state->arg_num < 1)
+            /* Not enough arguments. */
+            argp_usage (state);
+         break;
+
+      default:
+         return ARGP_ERR_UNKNOWN;
+   }
+
+   return 0;
 }
 
 void print_endpoint(struct usb_endpoint_descriptor *endpoint)
@@ -131,7 +183,7 @@ void print_configuration(struct usb_config_descriptor *config)
     print_interface(&config->interface[i]);
 }
 
-int print_device_orig(struct usb_device *dev, int level)
+int print_device_orig(struct usb_device *dev, int level, int verbose)
 {
   usb_dev_handle *udev;
   char description[256];
@@ -183,13 +235,13 @@ int print_device_orig(struct usb_device *dev, int level)
       print_configuration(&dev->config[i]);
   } else {
     for (i = 0; i < dev->num_children; i++)
-      print_device(dev->children[i], level + 1);
+       print_device_orig(dev->children[i], level + 1, verbose);
   }
 
   return 0;
 }
 
-int print_device(struct usb_device *dev, int level)
+int print_device(struct usb_device *dev, int level, int verbose)
 {
   usb_dev_handle *udev;
   char description[256];
@@ -199,8 +251,8 @@ int print_device(struct usb_device *dev, int level)
   udev = usb_open(dev);
   if (udev)
   {
-     i = snprintf(description, sizeof(description), "[%04X/%04X]",
-                  dev->descriptor.idVendor, dev->descriptor.idProduct);
+     i = snprintf(description, sizeof(description), "ID:%04X/%04X/%04X",
+                  dev->descriptor.idVendor, dev->descriptor.idProduct,dev->descriptor.bcdDevice);
 
      ret = usb_get_string_simple(udev, dev->descriptor.iManufacturer, string, sizeof(string));
      if (ret > 0)
@@ -212,7 +264,7 @@ int print_device(struct usb_device *dev, int level)
      ret = usb_get_string_simple(udev, dev->descriptor.iProduct, string, sizeof(string));
      if (ret > 0)
      {
-        ret = snprintf(&description[i], sizeof(description) - i, " :: %s", string);
+        ret = snprintf(&description[i], sizeof(description) - i, " - %s", string);
         i += ret;
      }
 #if 0
@@ -221,19 +273,37 @@ int print_device(struct usb_device *dev, int level)
             "BBB", "DDD",
             dev->devnum,
             description);
-#else
+
      printf ("Bus: %s Device: %s Dev#%d %s\n", dev->bus->dirname, dev->filename,
              dev->devnum,
              description);
+#else
+     {
+#ifdef LIBUSB_HAS_GET_DRIVER_NP
+        char buf[64];
+        int bInterfaceNumber, result;
+
+        bInterfaceNumber = dev->config->interface->altsetting[0].bInterfaceNumber;
+        result = usb_get_driver_np(udev, bInterfaceNumber, buf, 64);
+        if (!result)
+           sprintf (string, "Driver:%s ", buf);
+        else
+           string[0] = 0;
+#else
+        string[0] = 0;
+#endif
+     }
+     printf ("%s/%s/%s Dev:%d %s%s\n", PATH_USBFS,
+             dev->bus->dirname, dev->filename,
+             dev->devnum, string, description);
 #endif
   }
   if (udev && verbose) {
-    if (dev->descriptor.iSerialNumber) {
-      ret = usb_get_string_simple(udev, dev->descriptor.iSerialNumber, string, sizeof(string));
-      if (ret > 0)
-        printf("%.*s  - Serial Number: %s\n", level * 2,
-               "                    ", string);
-    }
+     if (dev->descriptor.iSerialNumber) {
+        ret = usb_get_string_simple(udev, dev->descriptor.iSerialNumber, string, sizeof(string));
+        if (ret > 0)
+           printf("%.*s  Serial Number:        %s\n", level * 2, "                    ", string);
+     }
   }
 
   if (udev)
@@ -246,43 +316,69 @@ int print_device(struct usb_device *dev, int level)
     }
 
     for (i = 0; i < dev->descriptor.bNumConfigurations; i++)
-      print_configuration(&dev->config[i]);
+       print_configuration(&dev->config[i]);
   } else {
-    for (i = 0; i < dev->num_children; i++)
-      print_device(dev->children[i], level + 1);
+//    for (i = 0; i < dev->num_children; i++)
+//       print_device(dev->children[i], level + 1, verbose);
   }
 
   return 0;
 }
 
-void print_devices (void)
+void print_devices (int verbose)
 {
   struct usb_bus *bus;
 
-  usb_find_busses();
-  usb_find_devices();
+  for (bus = usb_busses; bus; bus = bus->next)
+  {
+     if (bus->root_dev && !verbose)
+        print_device(bus->root_dev, 0, verbose);
+     else {
+        struct usb_device *dev;
 
-  for (bus = usb_busses; bus; bus = bus->next) {
-    if (bus->root_dev && !verbose)
-      print_device(bus->root_dev, 0);
-    else {
-      struct usb_device *dev;
-
-      for (dev = bus->devices; dev; dev = dev->next)
-        print_device(dev, 0);
-    }
+        for (dev = bus->devices; dev; dev = dev->next)
+           print_device(dev, 0, verbose);
+     }
   }
+}
+
+/* Clone dev and add head */
+int list_add_clone (struct usb_device **list, struct usb_device *dev)
+{
+   struct usb_device *new = calloc (1, sizeof(struct usb_device));
+
+   if (!new)
+   {
+      errx (ENOMEM, "Yikes! No memory ... bailing out.");
+   }
+
+   /* Found a match, clone and put it on the list */
+   bcopy (dev, new, sizeof (struct usb_device));
+   new->prev = new->next = NULL;
+
+   if (!*list)
+   {
+      *list = new;
+   }
+   else
+   {
+      new->next     = *list;
+      (*list)->prev = new;
+      *list         = new;
+   }
+
+   return 0;
 }
 
 struct usb_device *find_devices (int vid, int pid, int did)
 {
   struct usb_bus *bus;
-  struct usb_device *head = NULL, *curr;
+  struct usb_device *head = NULL;
 
   usb_find_busses();
   usb_find_devices();
 
-  printf ("%s() - Searching for 0x%04X/0x%04X\n", __FUNCTION__, vid, pid);
+  //printf ("%s() - Searching for 0x%04X/0x%04X\n", __FUNCTION__, vid, pid);
 
   for (bus = usb_busses; bus; bus = bus->next)
   {
@@ -292,23 +388,7 @@ struct usb_device *find_devices (int vid, int pid, int did)
      {
         if (dev->descriptor.idVendor == vid && dev->descriptor.idProduct == pid)
         {
-           struct usb_device *new = calloc (1, sizeof(struct usb_device));
-
-           /* Found a match, clone and put it on the list */
-           bcopy (dev, new, sizeof (struct usb_device));
-           new->prev = new->next = NULL;
-
-           if (!head)
-           {
-              head = new;
-              curr = head;
-           }
-           else
-           {
-              new->prev  = curr;
-              curr->next = new;
-              curr       = new;
-           }
+           list_add_clone (&head, dev);
         }
      }
   }
@@ -316,67 +396,99 @@ struct usb_device *find_devices (int vid, int pid, int did)
   return head;
 }
 
-
-int do_usb_reset (struct usb_device *dev)
+struct usb_device *locate_device (char *path)
 {
-   int result = -1;
-   int bInterfaceNumber;
-   usb_dev_handle *udev;
+   struct usb_device *dev = NULL;
 
-   udev = usb_open(dev);
-   if (udev)
+   list_add_clone (&dev, get_usb_device (path));
+
+   return dev;
+}
+
+int reset (struct usb_device *list, int verbose)
+{
+   int result = 0;
+   struct usb_dev_handle *udev;
+
+   while (list)
    {
-      bInterfaceNumber = dev->config->interface->altsetting[0].bInterfaceNumber;
-      //result = usb_claim_interface (udev, bInterfaceNumber);
-      result = 0;
-      if (result) goto exit;
+      udev = usb_claim_device (list);
+      if (!udev)
+      {
+         fprintf (stderr, "Failed claiming device: %s\n", usb_strerror());
+      }
+      else
+      {
+         //print_device (dev, 0, verbose);
+         printf ("Resetting ... ");
+         if ((result = usb_reset (udev)))
+         {
+            perror ("Failed");
+         }
+         else
+         {
+            printf ("OK\n");
+         }
 
-      printf ("Resetting!\n");
-      result = usb_reset (udev);
+         usb_release_device (udev);
+      }
 
-      //usb_release_interface (udev, bInterfaceNumber);
-     exit:
-      usb_close(udev);
+      list = list->next;
    }
 
    return result;
 }
 
-/* Our argp parser. */
-static struct argp argp = { options, parse_opt, args_doc, doc };
 
-int main (int argc, char **argv)
+/* Query device status using simple control ep */
+int status (struct usb_device *list, int verbose)
 {
-   int cmd, result;
-   struct usb_device *list, *dev;
-   struct arguments arguments;
+   int result;
+   struct usb_dev_handle *udev;
 
-   /* Default values. */
-   arguments.silent = 0;
-   arguments.verbose = 0;
-   arguments.device_path = getenv ("DEVICE");
-
-   /* Parse our arguments; every option seen by `parse_opt' will
-      be reflected in `arguments'. */
-   argp_parse (&argp, argc, argv, 0, 0, &arguments);
-   verbose = arguments.verbose;
-
-   usb_init();
-
-   //print_devices ();
-   verbose = 1;
-   //find_device (atoi(argv[1]), atoi(argv[2]), 0);
-   list = find_devices (0xE6E6, 0x201, 0);
-   dev = list;
-   while (dev)
+   while (list)
    {
-      print_device (dev, 0);
-      if (do_usb_reset (dev))
+      udev = usb_claim_device (list);
+      if (!udev)
       {
-         perror ("Failed USB device reset");
+         fprintf (stderr, "Failed claiming device: %s\n", usb_strerror());
       }
-      dev = dev->next;
+      else
+      {
+         result = usb_control_msg(udev, USB_TYPE_VENDOR | USB_RECIP_DEVICE,
+                                  USB_REQ_GET_STATUS, 0, 0, NULL, 0, 5000 );
+         if (result)
+            printf("usb_control_msg() returned %d (%s)\n", result, usb_strerror());
+         result = usb_control_msg(udev, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
+                                  USB_REQ_GET_STATUS, 0, 0, NULL, 0, 5000 );
+         if (result)
+            printf("usb_control_msg() returned %d (%s)\n", result, usb_strerror());
+
+         usb_release_device (udev);
+      }
+
+      list = list->next;
    }
+
+   return 0;
+}
+
+/* Display device information */
+int display (struct usb_device *list, int verbose)
+{
+   while (list)
+   {
+      print_device (list, 0, verbose);
+      list = list->next;
+   }
+
+   return 0;
+}
+
+int list_free (struct usb_device *list)
+{
+   struct usb_device *dev;
+
    while (list)
    {
       dev = list;
@@ -384,6 +496,106 @@ int main (int argc, char **argv)
 
       free (dev);
    }
+
+   return 0;
+}
+
+typedef enum {DISPLAY = 0, STATUS, RESET} op_t;
+
+typedef struct {
+  char *command;
+  op_t  cmd;
+} cmd_t;
+
+cmd_t command_map[] = {
+   {"DISPLAY", DISPLAY},
+   {"SHOW", DISPLAY},
+   {"STATUS", STATUS},
+   {"RESET", RESET},
+};
+
+#define ARRAY_SIZE(a) sizeof((a)) / sizeof((a)[0])
+
+int map_command_to_cmd (char *command)
+{
+  int i;
+
+  for (i = 0; i < ARRAY_SIZE(command_map); i++)
+    {
+      if (!strcasecmp (command_map[i].command, command))
+        return command_map[i].cmd;
+    }
+
+  return -1;
+}
+
+
+
+int main (int argc, char **argv)
+{
+   int cmd;
+   struct usb_device *list;
+   struct arguments arg;
+   /* Our argp parser. */
+   static struct argp argp = { options, parse_opt, args_doc, doc };
+
+   /* Default values. */
+   arg.silent  = 0;
+   arg.verbose = 0;
+   arg.vid     = 0;
+   arg.pid     = 0;
+   arg.path    = getenv ("DEVICE");
+   arg.cmd[0]  = "DISPLAY";
+
+   /* Parse our arguments; every option seen by `parse_opt' will
+      be reflected in `arguments'. */
+   argp_parse (&argp, argc, argv, 0, 0, &arg);
+
+   usb_init();
+
+   cmd = map_command_to_cmd (arg.cmd[0]);
+   if (cmd < 0)
+   {
+      err(EINVAL, "No such command, reverint to display device.");
+   }
+
+   usb_find_busses();
+   usb_find_devices();
+
+   //print_devices ();
+   //find_device (atoi(argv[1]), atoi(argv[2]), 0);
+   //list = find_devices (0xE6E6, 0x201, 0);
+   if (arg.path)
+   {
+      list = locate_device (arg.path);
+   }
+   else if (arg.vid)
+   {
+      list = find_devices (arg.vid, arg.pid, 0);
+   }
+   else // (!arg.path && !arg.vid)
+   {
+      errx(EINVAL, "You must specify a device path or VID[/PID] device match.");
+   }
+
+   switch (cmd)
+   {
+      case STATUS:
+         status (list, arg.verbose);
+         break;
+
+      case RESET:
+         reset (list, arg.verbose);
+         break;
+
+      case DISPLAY:
+      default:
+         /* Read usb_device_descriptor and print it out. */
+         display (list, arg.verbose);
+         break;
+   }
+
+   list_free (list);
 
    return 0;
 }
